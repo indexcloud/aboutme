249e8ef99989dfd5dcbee6aaef7e1c6feaf5aeab
Jason Deng <foralief@gmail.com>
2021-03-02 21:37:19 -0600

    Adjust main.js setting

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,6 @@ let themeDots = document.getElementsByClassName("theme-dot");
 for (var i = 0; themeDots.length > i; i++) {
 	themeDots[i].addEventListener("click", function () {
 		let mode = this.dataset.mode;
-		console.log("Option clicked:", mode);
 		setTheme(mode);
 	});
 }
